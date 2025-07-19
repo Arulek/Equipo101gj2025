89c2b680057f77d0550f8d74150478af5985902f
Pablo Avalos <pabloavalos091@gmail.com>
2025-07-19 13:13:04 -0300

    animacion de movimiento y prefabs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     public float speed = 5f; // Speed of the player movement
     private InputSystem_Actions playerAction; // Input action for player movement
     private Rigidbody2D rb; // Rigidbody component for physics interactions
+
+    public Animator animator; // Animator component for player animations
     
     private void Awake()
     {
@@ -41,6 +43,37 @@ public class Player : MonoBehaviour
         input = input.normalized; // Normalize the input vector to ensure consistent speed in all directions)
         Vector3 movement = new Vector3(input.x, input.y, 0) * speed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + (Vector2)movement);
+
+        if (input == Vector2.zero)
+        {
+            animator.Play("Idle"); // Play idle animation if no input is detected
+            return;
+        }
+
+        if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+        {
+            if (input.x > 0)
+            {
+                // animator.SetTrigger("walkRight"); // Trigger right movement animation
+                animator.Play("WalkRight");
+            }
+            else
+            {
+                animator.Play("WalkLeft"); // Trigger left movement animation
+            }
+        }
+        if (Mathf.Abs(input.y) > Mathf.Abs(input.x))
+        {
+            if (input.y > 0)
+            {
+                animator.Play("walkUp"); // Trigger upward movement animation
+            }
+            else
+            {
+                animator.Play("walkDown"); // Trigger downward movement animation
+            }
+        }
+
         // rb.linearVelocity = new Vector2(input.x * speed, input.y * speed); // Set the velocity of the Rigidbody2D for movement
     }
 }
